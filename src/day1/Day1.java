9af5a7cbedcc1cf04373b799f6860f0574fc214a
Elliott Whyman <elliottwhyman@hotmail.co.uk>
2016-12-01 22:11:14 +0000

    Day 1

---
+++ src/day1/Day1.java
@@ -0,0 +1,95 @@
+package day1;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day1 {
+
+    private static Direction direction = Direction.NORTH;
+    private static Vector position = Vector.valueOf(0,0);
+    private static List<Vector> visited = new ArrayList<>();
+    private static boolean visitedTwice = false;
+    private static Vector visitedTwiceVector;
+
+    public static void main(String[] args) {
+        try {
+            String instructions = Files.lines(Paths.get(Day1.class.getResource("input.txt").toURI())).findFirst().get();
+            for (String string: instructions.split(",")) {
+                string = string.trim();
+                direction = direction.cycle(string.charAt(0));
+                int magnitude = Integer.parseInt(string.substring(1));
+                visitedTwice(magnitude);
+                position = position.add(direction.getVector().multiply(magnitude));
+            }
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("End: " + position.toString());
+        System.out.println("Length: " + (Math.abs(position.getCartesianX()) + Math.abs(position.getCartesianY())));
+        System.out.println("Visited Twice: " + visitedTwiceVector);
+        System.out.println("Visited Twice Length: " + (Math.abs(visitedTwiceVector.getCartesianX()) + Math.abs(visitedTwiceVector.getCartesianY())));
+
+    }
+
+    private static void visitedTwice(int magnitude) {
+        Vector vector = position;
+        for (int i = 1; i <= magnitude; i++) {
+            if (!visitedTwice) {
+                vector = vector.add(direction.getVector().multiply(1));
+                if (visited.contains(vector)) {
+                    visitedTwiceVector = vector;
+                    visitedTwice = true;
+                }
+                visited.add(vector);
+            }
+            System.out.println(vector);
+        }
+    }
+
+    enum Direction {
+
+        NORTH(Vector.valueOf(0,1), 0),
+        EAST(Vector.valueOf(1,0), 1),
+        SOUTH(Vector.valueOf(0,-1), 2),
+        WEST(Vector.valueOf(-1,0), 3);
+
+        Vector vector;
+        int value;
+
+        Direction(Vector vector, int value) {
+            this.vector = vector;
+            this.value = value;
+        }
+
+        int getValue() {
+            return value;
+        }
+
+        Vector getVector() {
+            return vector;
+        }
+
+        private static Direction getByValue(int value) {
+            for (Direction direction : values()) {
+               if (direction.getValue() == value)
+                   return direction;
+            }
+            return null;
+        }
+
+       Direction cycle(char cycle) {
+           if (cycle == 'R')
+               return getByValue((this.getValue() + 1) > 3 ? 0 : this.getValue() + 1);
+           if (cycle == 'L')
+               return getByValue((this.getValue() - 1) < 0 ? 3 : this.getValue() - 1);
+           return this;
+       }
+
+    }
+
+}
---
+++ src/day1/Vector.java
@@ -0,0 +1,85 @@
+package day1;
+
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Table;
+
+public class Vector {
+
+    private static Table<Integer, Integer, Vector> vectorTable = HashBasedTable.create();
+
+    private final int x;
+    private final int y;
+
+    private Vector(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Vector add(Vector vector) {
+        return Vector.valueOf(this.x + vector.x, this.y + vector.y);
+    }
+
+    public Vector subtract(Vector vector) {
+        return Vector.valueOf(this.x - vector.x, this.y - vector.y);
+    }
+
+    public Vector multiply(Vector vector) {
+        return Vector.valueOf(this.x * vector.x, this.y * vector.y);
+    }
+
+    public Vector multiply(int constant) {
+        return Vector.valueOf(this.x * constant, this.y * constant);
+    }
+
+    public Vector divide(Vector vector) {
+        return Vector.valueOf(this.x / vector.x, this.y / vector.y);
+    }
+
+    public double magnitudeSquared() {
+        return Math.pow(this.x, 2) + Math.pow(this.y, 2);
+    }
+
+    public Vector normaliseAndRound() {
+        int x = (int) Math.ceil(this.x / Math.sqrt(this.magnitudeSquared()));
+        int y = (int) Math.ceil(this.y / Math.sqrt(this.magnitudeSquared()));
+        return Vector.valueOf(x, y);
+    }
+
+    public int getCartesianX() {
+        return this.x;
+    }
+
+    public int getCartesianY() {
+        return this.y;
+    }
+
+    public static Vector valueOf(int x, int y) {
+        Vector vector = vectorTable.get(x,y);
+        if (vector != null)
+            return vector;
+        vectorTable.put(x,y, new Vector(x,y));
+        return valueOf(x,y);
+    }
+
+    @Override
+    public String toString() {
+        return "Vector(" + x + ", " + y + ")";
+    }
+
+    @Override
+    public int hashCode() {
+        return x * 31 + y;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object instanceof Vector) {
+            Vector Vector = (Vector) object;
+            if (Vector.getCartesianX() == this.getCartesianX() && Vector.getCartesianY() == this.getCartesianY()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
